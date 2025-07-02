caa07f8cf631f4de12c6c09b09566afc9a4893c3
崔雄雄 <cuixiongxiong>
2025-07-02 21:52:37 +0800

    tijiao

--- index.js
+++ index.js
@@ -5,26 +5,31 @@
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.get('/api/:date?', (req, res) => {
+  let date;
+  const dateParam = req.params.date;
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+  if (!dateParam) {
+    date = new Date();
+  } else if (!isNaN(dateParam)) {
+    date = new Date(parseInt(dateParam));
+  } else {
+    date = new Date(dateParam);
+  }
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
-
-
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
